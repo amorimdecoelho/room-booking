855faf61626866de595c8eb6d0d68070c2b634c7
Gabriel Coelho <amorimdecoelho@gmail.com>
2018-07-22 18:19:33 +0200

    Initial commit with Spring Boot seed

---
+++ src/main/java/com/mls/roombooking/RoombookingApplication.java
@@ -0,0 +1,12 @@
+package com.mls.roombooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoombookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoombookingApplication.class, args);
+	}
+}
